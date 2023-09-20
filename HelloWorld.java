350e2ca50e66a91e0a27654e5d5b062ca8bbcf33
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-20 11:15:04 -0700

    ms moy help mememememe

---
+++ HelloWorld.java
@@ -0,0 +1,30 @@
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        for (int i=0; i >= 15; i++) {
+            System.out.println("i is equal to " +i);
+        }
+         
+        for (int j=10; j == 0 ; j = j - 1) {
+            System.out.println("j is equal to " + j);
+        }
+        
+        for (int k=10; k == 0 ; k = k - 3) {
+            System.out.println("k is equal to " + k);
+        }
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
+
