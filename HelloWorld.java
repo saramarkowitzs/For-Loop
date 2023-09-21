23bea97c7a0ccc774f77304ada7b2eb5eb0004ec
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-21 11:20:30 -0700

    ewigf uhfejk

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,15 +7,15 @@ public class HelloWorld
     int d = -8;
     
     public void run() {
-        for (int i=0; i >= 15; i++) {
-            System.out.println("i is equal to " +i);
+         for (int i=0; i <= 15; i++) {
+            System.out.println("i is equal to " + i);
         }
-         
-        for (int j=10; j == 0 ; j = j - 1) {
+        
+        for (int j=10; j >= 0 ; j = j + (- 2)) {
             System.out.println("j is equal to " + j);
         }
         
-        for (int k=10; k == 0 ; k = k - 3) {
+        for (int k=10; k >= 0 ; k = k +  (- 3)) {
             System.out.println("k is equal to " + k);
         }
     }
